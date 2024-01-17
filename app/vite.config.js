fe53d3e257b431f96e4a6850c44e7ae6eb8e526c
cyph404 <nicoleblanchette05@gmail.com>
2024-01-17 18:51:17 -0500

    deployment prep

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     kaboom: 'kaboom'
-  //   }
-  // },
-
-  // Add optimizeDeps.include to include "kaboom" in the optimized dependencies
-  // resolve: {
-  //   alias: {
-  //     'kaboom': path.resolve(__dirname, '/kaboom/dist/kaboom.esm.min.js')
-  //   }
-  // // },
-  // optimizeDeps: {
-  //   include: ['kaboom']
-  // },
-
-  build: { outDir: '..' },
-
-  base: '/flappy-bird/', 
-});
-
+	build: { outDir: ".." },
+	base: "/flappy-bird/",
+})
 
 // Add resolve.alias to map "kaboom" to the Kaboom.js library
